65c3cda132ec1380105091474dc7af1fb8bf6bd6
MarcoValeri <marcovaleri@hotmail.it>
2025-05-02 16:25:29 +0100

    Add Firebase config

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,20 +1,8 @@
-interface NewsArticlePageProps {
-    params: {
-        slug: string;
-    }
+export default async function NewsArticle({
+    params,
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    const { slug } = await params
+    return <div>News Article with URL: {slug}</div>
 }
-
-const NewArticlePage = ({ params }: NewsArticlePageProps) => {
-    const { slug } = params;
-
-    // TODO: Fetch news data
-
-    return (
-        <div>
-            <h1>News Article: {slug}</h1>
-            <p>Content for the news articles</p>
-        </div>
-    )
-}
-
-export default NewArticlePage;
