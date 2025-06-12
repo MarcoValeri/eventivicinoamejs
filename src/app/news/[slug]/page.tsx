62bef214fede92bb5100cbf50d403a033b69ec1f
MarcoValeri <marcovaleri@hotmail.it>
2025-06-12 22:22:41 +0100

    Fix firebasekeys

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -4,5 +4,7 @@ export default async function NewsArticle({
     params: Promise<{ slug: string }>
 }) {
     const { slug } = await params
-    return <div>News Article with URL: {slug}</div>
+    return (
+        <div>News Article with URL: {slug}</div>
+    )
 }
